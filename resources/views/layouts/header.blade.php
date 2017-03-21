d9b627d5df790a1dfea5d49f1a339e4fb2e490b0
garik7012 <garik7012@mail333.com>
2017-03-21 16:23:42 +0200

    fix move link ios

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -30,10 +30,23 @@
         $(document).ready(function(){
             var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
             if(iOS) {
-                $('a').on('click touchend', function (e) {
-                    var el = $(this);
-                    var link = el.attr('href');
-                    window.location = link;
+                $('a').on('touchmove touchend', function(e) {
+
+                    // if touchmove>touchend, set the data() for this element to true. then leave touchmove & let touchend fail(data=true) redirection
+                    if (e.type == 'touchmove') {
+                        $.data(this, "touchmove_cancel_redirection", true );
+                        return;
+                    }
+
+                    // if it's a simple touchend, data() for this element doesn't exist.
+                    if (e.type == 'touchend' && !$.data(this, "touchmove_cancel_redirection")) {
+                        var el = $(this);
+                        var link = el.attr('href');
+                        window.location = link;
+                    }
+
+                    // if touchmove>touchend, to be redirected on a future simple touchend for this element
+                    $.data(this, "touchmove_cancel_redirection", false );
                 });
             }
         })
